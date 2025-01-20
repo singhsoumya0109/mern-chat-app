2f54b5fe31ff305927067c1d2fc46a425acf402c
singhsoumya0109 <singhsoumya1103@gmail.com>
2025-01-20 21:35:10 +0530

    code cleaned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,4 @@
-// import './App.css';
-// import { Route } from "react-router-dom"
-// import Homepage from "./pages/Homepage.js";
-// import Chatpage from "./pages/Chatpage.js";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Route path="/" component={Homepage} exact/>
-//       <Route path="/chats" component={Chatpage} />
-//     </div>
-//   );
-// }
-
-// export default App;
- import './App.css';
+import './App.css';
 import { Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.js";
 import Chatpage from "./pages/Chatpage.js";
--- frontend/src/context/ChatProvider.js
+++ frontend/src/context/ChatProvider.js
@@ -1,29 +1,3 @@
-// import { createContext,useContext ,useState,useEffect} from "react";
-// import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-// const ChatContext = createContext();
-
-// const ChatProvider = ({ children }) => {
-//     const history = useHistory();
-//     useEffect(() => {
-//         const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-//         setUser(userInfo);
-//         if (!userInfo) {
-//             history.push("/login");
-//         }
-//     }, [history]);
-//     const [user, setUser] = useState();
-//     return <ChatContext.Provider value={{user,setUser}}>
-//         {children}
-//     </ChatContext.Provider>;
-// };
-
-
-// export const ChatState = () => {
-//     return useContext(ChatContext);
-// };
-
-
-// export default ChatProvider;
 
 
 import React, { createContext, useContext, useState, useEffect } from "react";
